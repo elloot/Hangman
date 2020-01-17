7284d422df69b474142bd5a58768815ce83da0cc
elloot <elloot@github.com>
2020-01-17 11:03:40 +0100

    add random word generator

---
+++ src/word.java
@@ -0,0 +1,30 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * Class description
+ * 2020-01-17
+ * Author: Elliot Duchek
+ */
+public class word {
+    public String getWord () {
+        Scanner in;
+
+        try {
+            in = new Scanner(new File("./resources/words.txt"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            in = new Scanner(System.in);
+        }
+
+        ArrayList<String> words = new ArrayList<>();
+        while (in.hasNextLine()) {
+            words.add(in.nextLine());
+        }
+
+        return words.get((int) (Math.random()*words.size()));
+    }
+}
