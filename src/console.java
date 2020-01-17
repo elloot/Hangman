72a35bbec2b4fa2ab57a94fdd388920c3bf72050
elloot <elloot@github.com>
2020-01-17 10:23:54 +0100

    add man, class for handling console and main class

---
+++ src/console.java
@@ -0,0 +1,18 @@
+/**
+ * Class description
+ * 2020-01-17
+ * Author: Elliot Duchek, Tobias Sandström
+ */
+public class console {
+    public void drawMan(int guesses){
+        switch (guesses) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 6:
+                break;
+        }
+    }
+
+}
---
+++ src/hangman.java
@@ -0,0 +1,9 @@
+/**
+ * Class description
+ * 2020-01-17
+ * Author: Elliot Duchek, Tobias Sandström
+ */
+public class hangman {
+    public static void main(String[] args) {
+    }
+}
