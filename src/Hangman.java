d02cf6be236bbd6bbe173fdc849a631e2525ce30
elloot <elloot@github.com>
2020-01-27 13:52:17 +0100

    update wincheck, now decent. create method for showing game status

--- src/Hangman.java
+++ src/Hangman.java
@@ -14,7 +14,6 @@ class Hangman {
         int numGuesses = 6;
         ArrayList<Character> wrongGuesses = new ArrayList<>();
         ArrayList<String> guesses = new ArrayList<>();
-        boolean guesserWin = false;
 
         //gamemode selection
         System.out.print("Type 1 to play against an advanced AI and type 2 to play against a friend: ");
@@ -49,37 +48,53 @@ class Hangman {
             if (guessType.equalsIgnoreCase("letter")) {
                 char cGuess = guess.charAt(0);
                 numGuesses = w.checkGuessLetter(staticWord, visibleWord, cGuess, numGuesses, wrongGuesses);
-                Console.clearConsole();
-                c.drawMan(numGuesses);
-                c.drawWord(visibleWord, wrongGuesses);
+                drawGameStatus(c, numGuesses, wrongGuesses, visibleWord);
             } else if (guessType.equalsIgnoreCase("word")) {
                 numGuesses = w.checkGuessWord(staticWord, guess, numGuesses);
-                Console.clearConsole();
-                c.drawMan(numGuesses);
-                c.drawWord(visibleWord, wrongGuesses);
+                drawGameStatus(c, numGuesses, wrongGuesses, visibleWord);
             } else {
                 System.out.println("This guess is invalid");
             }
 
             //checks if there are no guesses left, if so picker wins
-            if (numGuesses == 0) {
+            if (pickerWin(numGuesses, staticWord, visibleWord)) {
+                c.drawWord(visibleWord, wrongGuesses);
                 System.exit(1337);
             }
 
             //checks if the guesser has guessed the word correctly, if so the guesser wins
-            for (int i = 0; i < staticWord.length(); i++) {
-                if (visibleWord.get(i).equals(staticWord.charAt(i))) {
-                    guesserWin = true;
-                } else if (!visibleWord.get(i).equals(staticWord.charAt(i))) {
-                    guesserWin = false;
-                }
-            }
+             if (guesserWin(staticWord, visibleWord)) {
+                 Console.drawWin();
+             }
 
-            if (guesserWin) {
-                Console.drawWin();
-            }
 
         }
+    }
 
+    private static void drawGameStatus(Console c, int numGuesses, ArrayList<Character> wrongGuesses, ArrayList<Character> visibleWord) {
+        Console.clearConsole();
+        c.drawMan(numGuesses);
+        c.drawWord(visibleWord, wrongGuesses);
+    }
+
+    private static boolean pickerWin(int numGuesses, String staticWord, ArrayList<Character> visibleWord) {
+        if (numGuesses == 0) {
+            for (int i = 0; i < staticWord.length(); i++) {
+                visibleWord.add(i, staticWord.charAt(i));
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+
+    private static boolean guesserWin(String staticWord, ArrayList<Character> visibleWord) {
+        for (int i = 0; i < staticWord.length(); i++) {
+            if (!visibleWord.get(i).equals(staticWord.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
     }
 }
